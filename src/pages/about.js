e71dfe1a7b074acdc8b6ea94fdeb32a56e1683be
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-07-01 18:58:41 +0300

    16

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About Me</h1>
+    <p>There are some random worlds</p>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/services.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+const Services = () => {
+  return (
+    <Layout>
+      <SEO title="Services" />
+      <h1>Services</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae inventore
+        minima rem nesciunt quisquam, qui nihil ab modi debitis quasi enim unde
+        ut illum amet nisi corporis, aliquam doloribus accusamus.
+      </p>
+    </Layout>
+  )
+}
+
+export default Services
